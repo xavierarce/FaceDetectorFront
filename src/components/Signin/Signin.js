c75b8bd4ba4aed8501dc7e3ace331178afa32434
xavierarce <119446640+xavierarce@users.noreply.github.com>
2023-08-08 11:06:48 -0500

    main

--- src/components/Signin/Signin.js
+++ src/components/Signin/Signin.js
@@ -20,7 +20,7 @@ class Signin extends React.Component {
 	}
 
 	onSubmitSignIn=()=>{
-		fetch('https://fr-backend-fz83.onrender.com',{
+		fetch('https://fr-backend-fz83.onrender.com/signin',{
 			method:'post',
 			headers:{'Content-Type':'application/json'},
 			body:JSON.stringify({
